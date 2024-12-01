dbd61a0609c90a9210fb164e6024e36a6df344b0
Ubuntu <ubuntu@ip-172-31-89-37.ec2.internal>
2024-12-01 04:09:09 +0000

    Initial commmit

--- generate_data.sh
+++ generate_data.sh
@@ -8,7 +8,15 @@ npm i -D serverless-dotenv-plugin
 echo "Instalando dependencias de Python..."
 pip install -r requirements.txt
 
-# 3. Ejecutar el script de Python
+# 3. Cargar las variables de entorno desde el archivo .env
+echo "Cargando variables de entorno desde el archivo .env..."
+source .env
+
+# 4. Ejecutar Serverless para desplegar las tablas DynamoDB con el stage desde las variables de entorno
+echo "Desplegando las tablas DynamoDB con Serverless..."
+sls deploy --stage $STAGE
+
+# 5. Ejecutar el script de Python para generar los datos
 echo "Ejecutando el script de Python para generar los datos..."
 python3 main.py
 
